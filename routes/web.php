8f1f33213dbdddce6f9699a6b63840e8b3bb3c03
furukyo <67f.kaw.t@gmail.com>
2020-05-30 20:51:00 +0900

    main

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,48 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Auth::routes();
+
+
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+// ログイン状態
+Route::group(['middleware' => 'auth'], function() {
+
+    // ユーザ関連
+    Route::resource('users', 'UsersController', ['only' => ['index', 'show', 'edit', 'update']]);
+
+    // フォロー/フォロー解除を追加
+    Route::post('users/{user}/follow', 'UsersController@follow')->name('follow');
+    Route::delete('users/{user}/unfollow', 'UsersController@unfollow')->name('unfollow');
+
+    // ツイート関連
+    Route::resource('tweets', 'TweetsController', ['only' => ['index', 'create', 'store', 'show', 'edit', 'update', 'destroy']]);
+
+
+    // ログイン状態
+Route::group(['middleware' => 'auth'], function() {
+
+    // 省略
+
+    // コメント関連
+    Route::resource('comments', 'CommentsController', ['only' => ['store']]);
+});
+});
+
+// ログイン状態
+Route::group(['middleware' => 'auth'], function() {
+
+    // 省略
+
+    // いいね関連
+    Route::resource('favorites', 'FavoritesController', ['only' => ['store', 'destroy']]);
+});
+
+
+
+/*Route::get('/', 'PagesController@getHome');
+Route::get('/about', 'PagesController@getAbout');
+Route::get('/contact', 'PagesController@getContact');
